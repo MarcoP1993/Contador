e709868d3c743947bed0270774da8ba79ac25808
Marco Antonio <perea_10@hotmail.com>
2020-10-07 18:01:43 +0200

    contador terminado con constraintLayout pantalla en horizontal

--- app/src/main/java/com/example/contador/MainActivity.java
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,8 +24,13 @@ public class MainActivity extends AppCompatActivity {
         contador++; //esto es un comentario
         if (contador == 100){
             texto_numero.setTextSize(150);
+            Toast.makeText(this, "Llevas 100", Toast.LENGTH_SHORT).show();
         }
         String nuevoNumero = String.valueOf(contador);
         texto_numero.setText(nuevoNumero);
+    }
+
+    public void mostrarMensaje(View view) {
+        Toast.makeText(this,"Hola a todos",Toast.LENGTH_SHORT).show();
     }
 }
